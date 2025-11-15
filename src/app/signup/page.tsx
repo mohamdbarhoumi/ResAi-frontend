6c97e1665b5ce06a8f4d6c085369bb800dcce0f8
mohamdbarhoumi <barhoumimohamed@fsg.u-gabes.tn>
2025-11-15 14:40:46 +0100

    Login Fix

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -31,7 +31,7 @@ export default function SignupPage() {
     const payload: UserPayload = { email, password };
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/signup`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/create`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
